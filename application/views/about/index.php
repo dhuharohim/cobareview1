2d5012358aa65900448488005142c4cda9f28107
gunturmjr <gunturmasputrajr@gmail.com>
2021-04-13 15:44:27 +0700

    about us

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -1,4 +1,40 @@
 <div class="container">
-    <h1 class="mt-4">About Me</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, perspiciatis. Cum praesentium dolore provident sapiente dolorum reiciendis iure quasi incidunt molestiae sint. Dolorem unde molestias quasi labore aut dolore ea.</p>
+    <h1 class="mt-4">About Us</h1>
+    <h4>Kami Hadir memberikan solusi untuk anda mencari kampus terbaik</h4>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">Nama</th>
+      <th scope="col">NIM</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Guntur Masputra Jr.</td>
+      <td>105218027</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Agung Muhammad R.</td>
+      <td>105218006</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Rido Nur Rahman</td>
+      <td>105218007</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Yeremi Andrew S.</td>
+      <td>105218039</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Lita Batara</td>
+      <td>105218053</td>
+    </tr>
+  </tbody>
+</table>
 </div>
