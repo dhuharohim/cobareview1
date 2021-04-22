f4aab3b42c89e8f6ad2e5b030f053297f692e68f
gunturmjr <gunturmasputrajr@gmail.com>
2021-04-22 11:22:30 +0700

    new detail

--- application/views/university/detail.php
+++ application/views/university/detail.php
@@ -5,7 +5,7 @@
                 <div class="card-header">
                     Detail Universitas
                 </div>
-              
+
                 <div class="card">
                     <img src="<?= base_url('assets/img/univ_logo/') . $universitas['logo']; ?>" class="img-thumbnail">
                     <div class="card-body">
@@ -14,9 +14,12 @@
                         <p class="card-text">Email : <?= $universitas['email']; ?></p>
                         <p class="card-text">Website : <a href="<?= $universitas['website']; ?>"><?= $universitas['website']; ?></a></p>
                         <p class="card-text">Telp : <?= $universitas['telp']; ?></p>
+                        <p class="card-text">Jumlah Lomba : <?= $universitas['jumlah_lomba']; ?></p>
+
                         <a href="<?= base_url(); ?>university" class="btn btn-primary">Kembali</a>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
